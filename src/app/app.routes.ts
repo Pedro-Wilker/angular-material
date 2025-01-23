6262fae89523b6760ab38f3080ca09af0062cb23
Pedro-Wilker <pedrowilker1516@gmail.com>
2025-01-23 13:09:44 -0300

    first version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+    {
+        path: '', pathMatch: 'full', redirectTo: 'dashboard'
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./contact/contact.component').then(c => c.ContactComponent),
+    },
+    {
+        path: 'new-contact',
+        loadComponent: () => import('./newcontact/newcontact.component').then(c => c.NewcontactComponent),
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+    },
+
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,15 +4,38 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [MatToolbarModule, MatButtonModule, MatIconModule,
-    MatSidenavModule, MatListModule
+    MatSidenavModule, MatListModule, FormsModule,
+    RouterModule, CommonModule
+
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  showFiller = false;
+  opened=false;
+
+  items = [
+    {
+      icon: 'pi pi-home',
+      label: 'Home',
+      routeLink: 'dashboard'
+    },
+    {
+      icon: 'pi pi-check',
+      label: 'Contact',
+      routeLink: 'contact',
+    },
+    {
+      icon: 'pi pi-check',
+      label: 'New Contact',
+      routeLink: 'new-contact'
+    },
+  ];
 }
